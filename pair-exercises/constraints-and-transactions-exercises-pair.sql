adbe06c2f9543c4ac30c1ab8683111b68f9477eb
Chris Perez <christopher12perez6@gmail.com>
2020-10-15 16:05:14 -0400

    Mod2Day4 Pairteam 4 Complete

--- pair-exercises/constraints-and-transactions-exercises-pair.sql
+++ pair-exercises/constraints-and-transactions-exercises-pair.sql
@@ -4,31 +4,71 @@
 -- 1. Add Superman's hometown, Smallville, Kansas to the city table. The 
 -- countrycode is 'USA', and population of 45001. (Yes, I looked it up on 
 -- Wikipedia.)
+INSERT INTO city (name, countrycode, district, population)
+VALUES ('Smallville', 'USA', 'Kansas', 45001);
 
 -- 2. Add Kryptonese to the countrylanguage table. Kryptonese is spoken by 0.0001
 -- percentage of the 'USA' population.
+INSERT INTO countrylanguage (countrycode, language, isofficial, percentage)
+VALUES ('USA', 'Kryptonese', false, 0.0001);
 
 -- 3. After heated debate, "Kryptonese" was renamed to "Krypto-babble", change 
 -- the appropriate record accordingly.
+UPDATE countrylanguage
+SET language = 'Krypto-babble'
+WHERE language = 'Kryptonese';
 
 -- 4. Set the US captial to Smallville, Kansas in the country table.
+UPDATE country
+SET capital = (SELECT id FROM city WHERE name = 'Smallville')
+WHERE code = 'USA';
+
+SELECT country.code, country.capital, city.name
+FROM city
+        JOIN country
+        ON country.capital = city.id
+WHERE city.name = 'Smallville';
 
 -- 5. Delete Smallville, Kansas from the city table. (Did it succeed? Why?)
+DELETE FROM city
+WHERE name = 'Smallville';
+-- Did not succeed because it violates the constraints of the key integrity
 
 -- 6. Return the US captial to Washington.
+UPDATE country
+SET capital = (SELECT id FROM city WHERE name = 'Washington')
+WHERE code = 'USA';
 
 -- 7. Delete Smallville, Kansas from the city table. (Did it succeed? Why?)
+DELETE FROM city
+WHERE name = 'Smallville';
+-- Succeeded because it is no longer referenced in the country table, which doesn't violate key integrity
 
 -- 8. Reverse the "is the official language" setting for all languages where the
 -- country's year of independence is within the range of 1800 and 1972 
 -- (exclusive). 
 -- (590 rows affected)
+UPDATE countrylanguage
+SET isofficial = NOT isofficial
+FROM country
+WHERE country.code = countrylanguage.countrycode 
+        AND country.indepyear > 1800
+        AND country.indepyear < 1972;
+
 
 -- 9. Convert population so it is expressed in 1,000s for all cities. (Round to
 -- the nearest integer value greater than 0.)
 -- (4079 rows affected)
+UPDATE city
+SET population = population / 1000;
 
 -- 10. Assuming a country's surfacearea is expressed in square miles, convert it to 
 -- square meters for all countries where French is spoken by more than 20% of the 
 -- population.
 -- (7 rows affected)
+UPDATE country
+SET surfacearea = surfacearea / 3.2808
+FROM countrylanguage cl
+WHERE cl.countrycode = code
+        AND cl.language LIKE '_rench'
+        AND cl.percentage > 20;
